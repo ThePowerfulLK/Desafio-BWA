8bf2096475bbedc9c085d29f6c9b3ce3c2841610
ThePowerfulLK <leo_leonardo192010@hotmail.com>
2022-02-09 01:47:48 -0300

    correção de codigo

--- BWA.py
+++ BWA.py
@@ -18,41 +18,40 @@ button = web.find_element(By.XPATH,'//button[text() = "Start" ]').click()
 
 #Função para varrer as linhas e colunas da tabela;
 for i, r in table.iterrows():        
-    role = r['Role in Company']
-    email = r['Email']
     first_name = r['First Name']
-    last_name = r['Last Name']
-    phone = r['Phone Number']
+   # last_name = r['Last Name']
     company = r['Company Name']
+    role = r['Role in Company']
     address = r['Address']
+    email = r['Email']
+    phone = r['Phone Number']
 
-    #Preencher First Name;
-    preenche.clear() 
+    #Preencher First Name; 
     preenche = web.find_element(By.XPATH,"//input[@ng-reflect-name='labelFirstName']")
     preenche.send_keys(first_name)
     #Preencher Last Name;
-    preenche.clear()
-    preenche = web.find_element(By.XPATH,"//input[@ng-reflect-name='labelLastName']")
-    preenche.send_keys(last_name)
+    
+   # preenche = web.find_element(By.XPATH,"//input[@ng-reflect-name='labelLastName']")
+   # preenche.send_keys(last_name)
     #Preencher Company Name;
-    preenche.clear()
+    
     preenche = web.find_element(By.XPATH,'//input[@ng-reflect-name="labelCompanyName"]')
     preenche.send_keys(company)
     #Preencher Role Company;
-    preenche.clear()
+    
     preenche = web.find_element(By.XPATH,'//input[@ng-reflect-name="labelRole"]')
     preenche.send_keys(role)
     #Preencher Adress;
-    preenche.clear()
-    web.find_element(By.XPATH,'//input[@ng-reflect-name="labelAddress"]')
+    
+    preenche = web.find_element(By.XPATH,'//input[@ng-reflect-name="labelAddress"]')
     preenche.send_keys(address)
     #Preencher Email;
-    preenche.clear()
-    web.find_element(By.XPATH,'//input[@ng-reflect-name="labelEmail"]')
+    
+    preenche = web.find_element(By.XPATH,'//input[@ng-reflect-name="labelEmail"]')
     preenche.send_keys(email)
     #Preencher Phone Number;
-    preenche.clear()
-    web.find_element(By.XPATH,'//input[@ng-reflect-name="labelPhone"]')
+    
+    preenche = web.find_element(By.XPATH,'//input[@ng-reflect-name="labelPhone"]')
     preenche.send_keys(phone)
     #Pressionar botão de envio de formulário;
     button = web.find_element(By.XPATH,'//input[@value = "Submit"]')
